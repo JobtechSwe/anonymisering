005ced62f9a5832de63e6a0c98bdc9a79b2864c1
penms <mattias.persson@arbetsformedlingen.se>
2020-10-26 15:27:27 +0100

    Moved files to python package, and data to resources folder.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='jobtech-anonymisering',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     package_data = {
-        'jobtech_anonymisering': ['names/*']
+        'jobtech-anonymisering': ['resources/names/*']
     },
     include_package_data=True,
     install_requires=[
